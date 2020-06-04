311e0bd98952fd7bf5f9f6633fc2df03fc37ca69
Eloy <eloydgutierrez@gmail.com>
2020-06-03 20:48:17 -0400

    added code to meet MVP

--- server.js
+++ server.js
@@ -58,15 +58,24 @@ server.post("/api/users", (req, res) => {
 server.put("/api/users/:id", (req, res) => {
     const user = db.getUserById(req.params.id)
 
-    if (user) {
+    if (user && req.body.name && req.body.bio) {
         const updatedUser =  db.updateUser(req.params.id, {
             name: req.body.name || user.name,
+            bio: req.body.bio || user.bio,
         })
 
-        res.json(updatedUser)
-    } else {
+        res.status(200).json(updatedUser)
+    } else if (!user) {
         res.status(404).json({
             message: "The user with the specified ID does not exist.",
+        })
+    } else if (!req.body.name || !req.body.bio){
+        res.status(400).json({
+            errorMessage: "Please provide name and bio for the user.",
+        })
+    } else {
+        res.status(500).json({
+            errorMessage: "The user information could not be modified.",
         })
     }
 })
@@ -76,9 +85,13 @@ server.delete("/api/users/:id", (req, res) => {
 
     if (user) {
         res.json(db.deleteUser(req.params.id))
-    } else {
+    } else if (!user) {
         res.status(404).json({
             message: "The user with the specified ID does not exist.",
+        })
+    } else {
+        res.status(500).json({
+            errorMessage: "The user could not be removed",
         })
     }
 })
